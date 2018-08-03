559f5aba30bcc644d2e5c6a8eee9b1da1dafd887
Lance Johnson <lance.johnson@betterup.co>
2018-08-03 13:25:47 -0500

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_030230) do
+ActiveRecord::Schema.define(version: 2018_08_03_174924) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2018_07_30_030230) do
     t.integer "credit_account_id"
     t.string "description"
     t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
