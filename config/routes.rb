155d3187cc83d6773791b9135044928157073b45
Lance Johnson <lance.johnson@betterup.co>
2018-08-03 17:04:25 -0500

    create initial login page and error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :transactions
   resources :accounts
   resources :users
