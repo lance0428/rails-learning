e42500d14d40bc5559250833dadc0de8f0da70ec
Lance Johnson <lance.johnson@betterup.co>
2018-08-03 13:53:54 -0500

    Add User password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_182613) do
+ActiveRecord::Schema.define(version: 2018_08_03_183758) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_08_03_182613) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -61,5 +62,47 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "LanceJohnson@SomewhereJFJKD.coM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password can be long" do
+    @user.password = @user.password_confirmation = "a" * 6
+    assert @user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "valid password authenticates" do
+    password = "myPassword"
+    @user.password = password
+    @user.password_confirmation = password
+    @user.save
+    @user.reload
+
+    assert @user.authenticate(password)
+  end
+
+  test "invalid password does not authenticate" do
+    password = "myPassword"
+    @user.password = password
+    @user.password_confirmation = password
+    @user.save
+    @user.reload
+
+    assert_not @user.authenticate("bad")
   end
 end
